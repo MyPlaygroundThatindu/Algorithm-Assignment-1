1ec736abd7ee178a3301d6c8cb39d9e038d71bf3
Tharindu Thennakoon <chanaka152@gmail.com>
2023-08-14 01:13:00 +0530

    Completed Algo_4

---
+++ Algo_4.java
@@ -0,0 +1,17 @@
+public class Algo_4 {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+
+        for(int i=0; i<nums.length/2; i++){
+            int temp = nums[i];
+            nums[i] = nums[nums.length-(i+1)]; 
+            nums[nums.length-(i+1)] = temp;   
+        }
+
+        //Print the array
+        for(int i=0; i<nums.length;i++){
+            System.out.print(nums[i] + ",");
+        }
+        System.out.println("\b ");
+    }
+}
