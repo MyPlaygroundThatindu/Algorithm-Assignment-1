639b9197730fc296ee1e72b189480d9cf8b3f7d3
Tharindu Thennakoon <chanaka152@gmail.com>
2023-08-13 23:09:06 +0530

    Completed Algo_1

---
+++ Algo_1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Algo_1{
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int num = 0;
+        int[] fabSeq = {};
+
+        do{
+            System.out.print("Input a positive integer : ");
+            num = scanner.nextInt();
+            scanner.nextLine();
+
+            if(num <= 0) System.out.println("Please enter a positive integer");
+            else {
+                fabSeq = new int[num];
+                fabSeq[0] = 0;
+                fabSeq[1] = 1;
+                for(int i=2; i<num; i++){
+                    fabSeq[i] = fabSeq[i-1] + fabSeq[i-2];
+                }
+            }
+
+        } while (num < 0);
+
+        for(int i=0; i<fabSeq.length;i++){
+            System.out.print(fabSeq[i] + ",");
+        }
+        System.out.println("\b ");
+        
+    }
+}
